6fe994305a0e6ab9b091bfb5937b2e41e977b65d
Eric Sanderson <ersander@microsoft.com>
2018-11-18 21:21:10 -0800

    PB_SerializeFields

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
+
     Rigidbody rigidBody;
     AudioSource rocketThrustSound;
     
@@ -24,10 +28,11 @@ public class Rocket : MonoBehaviour {
 
     private void Thrust()
     {
+        float thrustThisFrame = mainThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.Space))
         {
             print("Thrusting"); // can thrust while rotating
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
             if (!rocketThrustSound.isPlaying)
             {
                 rocketThrustSound.Play();
@@ -43,15 +48,18 @@ public class Rocket : MonoBehaviour {
     {
         rigidBody.freezeRotation = true; // take manual control of rotation
 
+       
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
             print("Rocket Rotate LEFT");
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             print("Rocket Rotate RIGHT");
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
         rigidBody.freezeRotation = false; // resume physics control of rotation
