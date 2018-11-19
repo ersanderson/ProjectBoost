cca2850ab0065913fa08b201170cf6e749c14ccd
Eric Sanderson <ersander@microsoft.com>
2018-11-18 20:51:24 -0800

    PB_MovementRefine_CodeCleanup

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,27 +6,42 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
-
-	// Use this for initialization
+    AudioSource rocketThrustSound;
+    
+    // Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
-        
-		
-	}
+        rocketThrustSound = GetComponent<AudioSource>();
+        rocketThrustSound.Stop();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
-	}
+        Thrust();
+        Rotate();
+    }
 
-    private void ProcessInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             print("Thrusting"); // can thrust while rotating
             rigidBody.AddRelativeForce(Vector3.up);
-            
+            if (!rocketThrustSound.isPlaying)
+            {
+                rocketThrustSound.Play();
+            }
         }
+        else
+        {
+            rocketThrustSound.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
 
         if (Input.GetKey(KeyCode.A))
         {
@@ -38,5 +53,10 @@ public class Rocket : MonoBehaviour {
             print("Rocket Rotate RIGHT");
             transform.Rotate(-Vector3.forward);
         }
+
+        rigidBody.freezeRotation = false; // resume physics control of rotation
     }
+
+    
 }
+
